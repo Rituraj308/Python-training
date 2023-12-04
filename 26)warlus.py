cc58f4d3cbc8d79e2c03a05e191885a51169ea18
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2023-12-04 21:07:50 +0530

    Update 26)warlus.py

--- 26)warlus.py
+++ 26)warlus.py
@@ -1,4 +1,11 @@
-a='hellloooo'
+a='hellloooooooo'
 if (len(a) > 10):
-    print(f"too long {len(a)}elements")
- 
+    print(f"too long {len(a)} elements")
+#we r using 2 times 'len' in the code so to remove that we can use walrus opertor
+a='hellloooooooo'
+if ((n := len(a))>10):
+    print(f"too long {n} elements")
+while ((n := len(a))>1):
+    print(n)
+    a=a[:-1]
+print(a)
