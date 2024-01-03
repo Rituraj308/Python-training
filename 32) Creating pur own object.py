d8cf2f059600b80ad414955ce7c3646e62ab0cec
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-03 18:40:44 +0530

    Create 32) Creating pur own object.py

---
+++ 32) Creating pur own object.py
@@ -0,0 +1,13 @@
+class PlayerCharacter:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def run(self):
+        print('run')
+        return 'done'
+
+player1 = PlayerCharacter('Cindy',22)
+player2 = PlayerCharacter('Tom',34)
+player2.attack = 50
+print(player1.age)
+print(player2.age)
