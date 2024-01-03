c2300b052c8f841b9269dfddc25e890acf91ca79
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-03 16:52:17 +0530

    ..

---
+++ 31) what is oop .py
@@ -0,0 +1,16 @@
+#OOP
+class BigObject:    #class
+    pass
+
+obj1 = BigObject()   #instanciat
+obj2 = BigObject()   #instanciat
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([]))
+print(type(()))
+print(type({}))
+print(type(obj1))
+print(type(obj2))
