09ea82eb5a80159c7d76a66b1f2db69b3b94ea5b
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-09 18:19:46 +0530

    Create 37)pvt vs public variables.py

---
+++ 37)pvt vs public variables.py
@@ -0,0 +1,16 @@
+# we use '_' to make a variable private in python .Although it doesn't give any special power to variable ,but as a
+# python programmer we determine that if we see a '_' in the variable we should understand that it's a private variable
+# and it should't be modified .
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def speak(self):
+     print(f'my name is {self._name},and i am {self._age} years old.')
+
+Player1 = PlayerCharacter('Ritu Raj', 25)
+Player1.name = '!!!'
+Player1.speak = 'BOOOO'
+
+print(Player1.speak)
