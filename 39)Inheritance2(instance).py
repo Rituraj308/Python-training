c6b9d4762fa9353d3562c8626246e487e7ac2d6a
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-10 18:05:45 +0530

    Create 39)Inheritance2(instance).py

---
+++ 39)Inheritance2(instance).py
@@ -0,0 +1,25 @@
+class Usser():
+    def sign_in(self):
+        print('logged in')
+
+
+class Wizard(Usser):
+   def __init__(self, name, power):
+       self.name = name
+       self.power = power
+
+   def attack(self):
+        print(f'attacking with power of {self.power}')
+
+
+class Archer(Usser):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f'attacking with arrows: arrows left- {self.num_arrows}')
+
+wizard1 = Wizard('Marline', 60)
+print(isinstance(wizard1, Wizard))
+print(isinstance(wizard1, Usser))
