e530189e4af3a6cdc5931666a75409c99d92d25f
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-12 17:59:22 +0530

    Update 43)Dunder Methods.py

--- 43)Dunder Methods.py
+++ 43)Dunder Methods.py
@@ -2,17 +2,20 @@ class Toy():
     def __init__(self, color, age):
         self.color = color
         self.age = age
-        self.my_dict= {
-            'name' : 'Yoyo',
+        self.my_dict = {
+            'name': 'Yoyo',
             'has_pets': False
         }
 
     def __str__(self):
         return f'{self.color}'
+
     def __len__(self):
         return 5
+
     def __call__(self):
         return ('yess??')
+
     def __getitem__(self, i):
             return self.my_dict[i]
 
