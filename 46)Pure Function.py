6f04e850efbde9d69a91a1cc59b2991c366bf368
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-17 16:36:21 +0530

    Create 46)Pure Function.py

---
+++ 46)Pure Function.py
@@ -0,0 +1,15 @@
+def multiply_by2(li):
+    new_list = []
+    for item in li:
+        new_list.append(item*2)
+    return new_list
+
+print(multiply_by2([1,2,3]))
+
+def multiply_by2(li):
+    new_list = []
+    for item in li:
+        new_list.append(item*2)
+    return print(new_list)
+
+(multiply_by2([1,2,3]))
