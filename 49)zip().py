42a3c0c38d599d36e864f635404a53b9f54a9567
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-19 13:15:30 +0530

    Create 49)zip().py

---
+++ 49)zip().py
@@ -0,0 +1,12 @@
+# we can zip 2 lists or 2 itrables togather
+my_list = [1,2,3]
+your_list = [10,20,30]
+
+
+print(list(zip(my_list,your_list)))
+
+#Zip like zipper takes 2 or more ittrables and zip them togather, it doesn't  matter if it's a tupple.
+my_list2 = (3,4,5)
+your_list2 = (13,24,15)
+
+print(list(zip(my_list2,your_list2)))
