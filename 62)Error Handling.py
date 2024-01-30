a9df83f14f5b0035e0f9ca9ea5498dc4945c839a
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-01-30 19:57:05 +0530

    Create 62)Error Handling.py

---
+++ 62)Error Handling.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        age = int(input('what is your age? '))           # we use 'int' to allow code to accept only numbers
+        10/age
+    except ValueError:
+        print("please enter a number")
+    except ZeroDivisionError:
+        print("please enter a number higher than 0")
+    else:
+        print('thank you!')
+        break
+
+
+
