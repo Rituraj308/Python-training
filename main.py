b2e2e48c392b05655f75905211444ebb9308494a
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-02-11 19:46:15 +0530

    connecting one file to another
    here we are connecting the utility.py file to main.py file with utility function in utility.py file

---
+++ main.py
@@ -0,0 +1,5 @@
+# in this section we learned how to import function of one file to another file , for eg here we imorted utility function
+#from uitlity.py file to main.py file
+import utility
+
+print(utility.multiply(2,3))
