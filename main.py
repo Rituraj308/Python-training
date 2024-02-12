5010136dada096b3c11065b875ad79a1daa31e34
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-02-12 12:25:25 +0530

    creating a new directory or python package
    here we learn how to create new directory or python package and importing the function to the different file.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # in this section we learned how to import function of one file to another file , for eg here we imorted utility function
-#from uitlity.py file to main.py file
+#from uitlity.py file to main.py file and how to import a function from other directory/pythonpackg to other directory's file
 import utility
+import shopping.shopping_cart
 
 print(utility.multiply(2,3))
+print(shopping.shopping_cart.buy('apple'))
