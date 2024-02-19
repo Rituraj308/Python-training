9a20376c6b1352c5f36c30eb6c507563ff42744a
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-02-19 13:34:15 +0530

    .

--- 70)main.py
+++ 70)main.py
@@ -1,5 +1,5 @@
 # in this section we learned how to import function of one file to another file , for eg here we imorted utility function
-#from uitlity.py file to main.py file and how to import a function from other directory/pythonpackg to other directory's file
+#from uitlity.py file to 70)main.py file and how to import a function from other directory/pythonpackg to other directory's file
 from utility import multiply, divide
 from shopping.more_shopping.shopping_cart import buy
 
