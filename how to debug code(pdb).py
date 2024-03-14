5bed49c10c6202b020be1aa12ba8c523ca578306
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-03-14 18:14:38 +0530

    Update how to debug code(pdb).py

--- how to debug code(pdb).py
+++ how to debug code(pdb).py
@@ -1,7 +1,7 @@
-import pdb
+import pdb                   #pdb=python debuging (its a built-in module in python). it allows us to interact with the code
 def add(num1, num2):
     pdb.set_trace()
     t = 4*5
     return num1+num2
 
-add(2,'fyukjghjghj')
+add(4,'fyukjghjghj')
