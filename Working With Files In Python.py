56a544e40bf03e9af6d0e4bef330cb177939ccda
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-03-18 20:24:52 +0530

    Update Working With Files In Python.py

--- Working With Files In Python.py
+++ Working With Files In Python.py
@@ -1,3 +1,4 @@
-with open('tests.txt') as my_file:
-    print(my_file.readlines())
-#1st we have to creat a text file in notepad then save that file into the python file path 
+with open('tests.txt', mode='r') as my_file:
+    print(my_file.readline())
+    print(my_file.readline())
+#1st we have to creat a text file in notepad then save that file into the python file path
