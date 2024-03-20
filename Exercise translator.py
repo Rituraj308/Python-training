07d866ebdfdd23dc0d48918e5ee17fd60ada1a40
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-03-20 19:57:17 +0530

    Update Exercise translator.py

--- Exercise translator.py
+++ Exercise translator.py
@@ -1,8 +1,8 @@
-from translate import Translator
+from translate import Translator           # we have to install translator package from command prompt( by typing pip3 install translotr)
 translator = Translator(to_lang="ja")
-
+# we have to create text file or notepad file and write a sentence in it and save it in our pycharm path folder
 try:
-    with open('translator.txt', mode='r',encoding='utf-8') as my_file:
+    with open('translator.txt', mode='r',encoding='utf-8') as my_file:    # here we are use encoding='utf-8' because we were getting error
         text = my_file.read()
         translation = translator.translate(text)
         print(translation)
