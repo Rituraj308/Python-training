94a9536a2216c391f2991f3ac9670da89288988e
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-03-24 18:08:45 +0530

    Create Exercise Password validation.py

---
+++ Exercise Password validation.py
@@ -0,0 +1,15 @@
+#using Re creat a password checker
+#it should be 8 char long
+#it should contain any short of letters , nummbers and $%#@
+
+
+import re    #re= regular expression
+
+pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)") #r is a raw string,it ignores everything that python interpreter might interpret
+string = 'rituraj'
+pattern2 = re.compile(r"[A-Za-z0-9$%#@]{8,}\d")
+password = 'fsdhfsnkjldglkdh2343%$9'
+
+a=pattern.search(string)
+check = pattern2.fullmatch(password)
+print(check)
