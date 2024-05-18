3d09dde33abf842dd2269d347ef852f7cbb2431d
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-05-18 19:18:52 +0530

    images

--- images.py
+++ images.py
@@ -1,5 +1,10 @@
-from PIL import Image
-
-img = Image.open('./py/pokidesk/pikachu.jpg')
-
+# 1st install pillow from cmd
+from PIL import Image, ImageFilter
+img = Image.open( 'C:\\Users\\ritur\\OneDrive\\Desktop\\RITURAJ\\Git hub proj\\Python-training\\pokidesk\\pikachu.jpg')
 print(img)
+print(img.size)
+print(img.format)
+print(img.mode)
+print(dir(img))
+filtered_img = img.filter(ImageFilter.BLUR)
+filtered_img.save("blur.png", "png")
