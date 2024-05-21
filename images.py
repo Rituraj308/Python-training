22f61f8a192f7b98b84754d5699ff4a95d936a90
Rituraj308 <152595316+Rituraj308@users.noreply.github.com>
2024-05-21 18:58:21 +0530

    .

--- images.py
+++ images.py
@@ -6,5 +6,7 @@ print(img.size)
 print(img.format)
 print(img.mode)
 print(dir(img))
-filtered_img = img.filter(ImageFilter.BLUR)
+filtered_img = img.filter(ImageFilter.BLUR) # it makes the image blur
 filtered_img.save("blur.png", "png")
+filtered_img2 = img.convert('L') #it changes the clour of the image to grey(L)
+filtered_img2.save("grey.png", 'png')
